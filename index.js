31cc14c6c2ce4d2e2af1f2366a744a7375d9616d
Sean Sarokas <sps4462@gmail.com>
2022-05-19 10:01:57 -0400

    commit tree a1e862338900d600ea1aba4a7494318423701535

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let i = 0; i < word.length/2; i++){
+    const x = word.length -1 -i;
+    if(word[i] !== word[x]) return false
+  }
+  return true
 }
 
 /* 
   Add your pseudocode here
+  iterate through letters of word
+  if first letter equals last letter, true
+  if letters don't match, false
 */
 
 /*
   Add written explanation of your solution here
+  create a function that takes a word and returns true if the word
+  reads the same forewards as it does backwards, and false otherwise
 */
 
 // You can run `node index.js` to view these console logs
